35066e20553e916c36fda57aad371a6629d93004
REINNn0820 <nymaa4764@gmail.com>
2024-11-11 17:13:21 +0800

    Todo day 1

---
+++ todo.js
@@ -0,0 +1,32 @@
+const body = document.createElement("div");
+body.classList.add("body");
+
+const header = document.createElement("div");
+header.classList.add("header");
+console.log(body);
+document.body.appendChild(body);
+body.appendChild(header);
+const headerDot = document.createElement("div");
+headerDot.classList.add("headerDot");
+header.appendChild(headerDot);
+const headerP = document.createElement("p");
+headerP.classList.add("headerP");
+headerP.innerText = "TO DO";
+header.appendChild(headerP);
+const headerNumber = document.createElement("h4");
+headerNumber.classList.add("headerNumber");
+headerNumber.innerText = "5";
+header.appendChild(headerNumber);
+
+const main = document.createElement("div");
+main.classList.add("main");
+body.appendChild("main");
+
+function getName() {
+  let yourname = prompt();
+  document.getElementById("output").innerHTML = yourname;
+}
+function deleteName() {
+  let disappear = "";
+  document.getElementById("output").innerHTML = disappear;
+}
