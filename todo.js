3508b4ad6be19865b751b5bee29f55b383b43fb2
REINNn0820 <nymaa4764@gmail.com>
2024-11-11 17:42:03 +0800

    todo

--- todo.js
+++ todo.js
@@ -1,18 +1,24 @@
+const addTask = document.createElement("button");
+addTask.classList.add("addTask");
+
 const body = document.createElement("div");
 body.classList.add("body");
 
 const header = document.createElement("div");
 header.classList.add("header");
-console.log(body);
+
 document.body.appendChild(body);
 body.appendChild(header);
+
 const headerDot = document.createElement("div");
 headerDot.classList.add("headerDot");
 header.appendChild(headerDot);
+
 const headerP = document.createElement("p");
 headerP.classList.add("headerP");
 headerP.innerText = "TO DO";
 header.appendChild(headerP);
+
 const headerNumber = document.createElement("h4");
 headerNumber.classList.add("headerNumber");
 headerNumber.innerText = "5";
